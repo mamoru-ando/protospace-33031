9ee830a4451a9e0e9114624b8c82858f6a58eec5
mamoru-ando <bsirregulars@yahoo.co.jp>
2020-12-25 18:12:28 +0900

    ログイン時に他の投稿者の編集ページへ遷移しないようにした

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -25,6 +25,9 @@ class PrototypesController < ApplicationController
   end
 
   def edit
+    unless current_user.id == @prototype.user_id
+      redirect_to root_path
+    end
   end
 
   def update
